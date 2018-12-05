72dc1fa5b3be6410a2c15d67ac6a30cbf78deb06
Richard Moore <rich@optixsolutions.co.uk>
2018-12-05 12:01:47 +0000

    Truncate pagination

--- src/components/ui/Pagination.vue
+++ src/components/ui/Pagination.vue
@@ -1,15 +1,30 @@
 <template>
-    <nav class="pagination is-centered" role="navigation" aria-label="pagination" v-if="hasPages()">
-        <a class="pagination-previous" v-if="hasPreviousPage()" @click="changePage(previousPage)">Previous</a>
-        <span class="pagination-previous" disabled v-else>Previous</span>
+    <nav class="pagination is-centered" v-if="isVisible">
+        <a
+            class="pagination-previous"
+            :disabled="! hasPreviousPage"
+            @click="changePage(previousPage)"
+        >Prev</a>
 
-        <a class="pagination-next" v-if="hasNextPage()" @click="changePage(nextPage)">Next</a>
-        <span class="pagination-next" disabled v-else>Next</span>
+        <a
+            class="pagination-next"
+            :disabled="! hasNextPage"
+            @click="changePage(nextPage)"
+        >Next</a>
 
         <ul class="pagination-list">
-            <li :key="page" v-for="page in options.last_page">
-                <span class="pagination-link is-current" v-if="isCurrent(page)">{{ page }}</span>
-                <a class="pagination-link" @click="changePage(page)" v-else>{{ page }}</a>
+            <li :key="page" v-for="page in pages">
+                <span
+                    v-if="page === seperator"
+                    class="pagination-ellipsis"
+                >{{ seperator }}</span>
+
+                <a
+                    class="pagination-link"
+                    v-else
+                    @click="changePage(page + 1)"
+                    :class="{ 'is-current': page === currentPage }"
+                >{{ page + 1 }}</a>
             </li>
         </ul>
     </nav>
@@ -18,22 +33,37 @@
 <script>
     export default {
         props: {
-            options: Object
-        },
+            options: {
+                type: Object,
+                default: null
+            },
 
+            visiblePages: {
+                type: Number,
+                default: 3
+            },
+
+            seperator: {
+                type: String,
+                default: '...'
+            }
+        },
+        
         computed: {
+            isVisible() {
+                return this.options && this.options.last_page > 1;
+            },
+
+            currentPage() {
+                return this.options ? this.options.current_page - 1 : 0;
+            },
+
             previousPage() {
                 return this.options.current_page - 1;
             },
 
             nextPage() {
                 return this.options.current_page + 1;
-            }
-        },
-
-        methods: {
-            hasPages() {
-                return this.options && this.options.last_page > 1;
             },
 
             hasPreviousPage() {
@@ -44,12 +74,68 @@
                 return this.nextPage <= this.options.last_page;
             },
 
-            isCurrent(page) {
-                return this.options.current_page === page;
+            pages() {
+                let pages;
+                
+                if (this.filteredPages) {
+                    pages = [
+                        this.filteredPages[0] - 1 === 1 ? 1 : this.seperator,
+
+                        ...this.filteredPages,
+
+                        this.filteredPages[this.filteredPages.length - 1] + 1 === this.options.last_page - 2
+                            ? this.options.last_page - 2
+                            : this.seperator
+                    ];
+                } else {
+                    pages = [ ...Array(this.options.last_page - 2).keys() ].map(page => {
+                        return page + 1
+                    });
+                }
+
+                return [
+                    0,
+                    ...pages,
+                    this.options.last_page - 1
+                ];
             },
 
+            filteredPages() {
+                let toFilterPages = [...Array(this.options.last_page).keys()].slice(2, -2);
+
+                if (this.visiblePages > toFilterPages.length) {
+                    return null;
+                }
+
+                let diff = this.visiblePages / 2;
+                let diffFirst = this.currentPage - toFilterPages[0];
+                let diffLast = this.currentPage - toFilterPages[toFilterPages.length - 1];
+
+                if (diffFirst < diff) {
+                    return toFilterPages.slice(0, this.visiblePages);
+                }
+                
+                if (diffLast >= -diff) {
+                    return toFilterPages.slice(-this.visiblePages);
+                }
+
+                return toFilterPages.filter(page => {
+                    let diffPage = this.currentPage - page;
+
+                    return diffPage < 0
+                        ? Math.abs(diffPage) <= diff
+                        : diffPage < diff;
+                });
+            }
+        },
+
+        methods: {
             changePage(page) {
-                if (page < 1 || page > this.options.last_page) {
+                if (
+                    page < 1
+                    || page > this.options.last_page
+                    || page === this.options.current_page
+                ) {
                     return;
                 }
 
